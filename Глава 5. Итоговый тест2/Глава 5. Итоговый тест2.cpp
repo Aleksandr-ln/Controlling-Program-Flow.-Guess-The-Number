fee6651afba50d5b0f65da83f34df7fe591c481a
Dima <32909511+d1mmm@users.noreply.github.com>
2022-11-18 21:16:40 +0200

    Update Глава 5. Итоговый тест2.cpp

--- Глава 5. Итоговый тест2/Глава 5. Итоговый тест2.cpp
+++ Глава 5. Итоговый тест2/Глава 5. Итоговый тест2.cpp
@@ -1,23 +1,29 @@
-﻿#include <iostream>
+// MUST HAVE! //
+// Названия программы и папки кирилицей
+// В гите должно быть только код, остальные файлы, студия сама сгенерирует. Нужно добавить в гитигнор еще файлики
+// MUST HAVE! //
+
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
 
-int getRandomNumber(int min, int max)
+int getRandomNumber(int min, int max) // Нужно делать const int&. Ну ок, ты ссылки еще не знаешь, но const то знаешь)))) И поделай функции const
 {
     static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
    
     return static_cast<int>(rand() * fraction * (max - min + 1) + min);
 }
 
-void startingGame(int randNumb)
+void startingGame(int randNumb) // Тоже const&
 {
     randNumb = getRandomNumber(1, 100); // Вызов функции для получения нового рандомного числа, для каждой последующей игры
+    // Копированик кода getRandomNumber(). Сделай что бы ты раз ее вызывал
     std::cout << "Let's play a game. I'm thinking of a number. You have 7 tries to guess what it is.\n";
     int usersGuess;
-    for (int iii = 1; iii <= 7; iii++)
+    for (int iii = 1; iii <= 7; iii++) // зачем iii, i достаточно 
     {
-        std::cout << "Guess #" << iii << ":";
-        std::cin >> usersGuess;
+        std::cout << "Guess #" << iii << ":"; // вывод в главной фунции должен быть
+        std::cin >> usersGuess; // ввод тоже
         if (usersGuess > randNumb)
         {
             std::cout << "Your guess is too high.\n";
@@ -38,7 +44,8 @@ void startingGame(int randNumb)
     }
 }
 
-char requestForReplay(int randNumb)
+char requestForReplay(int randNumb) // тоже const&. Или указатели но & проще
+ // char? Зачем? Ты знаешь зачем тип char???
 {
     while (true)
     {
@@ -56,14 +63,14 @@ char requestForReplay(int randNumb)
             break;
         }
     }
-    return 0;
+    return 0; // Как return 0 вяжется к char?? 
 }
 
 int main()
 {
     srand(static_cast<unsigned int>(time(0))); // устанавка значения системных часов в качестве стартового числа
     rand(); // Первый вызов rand() для сброса результата этой функции (для большего отличия первого рандомного числа от стартового)
-    int randNumb = getRandomNumber(1, 100);
+    int randNumb = getRandomNumber(1, 100); // копирование кода
     
     startingGame(randNumb);
     requestForReplay(randNumb);
