846bfc44d27eff29d78115df8f2d0ad906dd4f43
Dima <32909511+d1mmm@users.noreply.github.com>
2022-11-25 13:31:59 +0200

    Update Глава 5. Итоговый тест2.cpp

--- Глава 5. Итоговый тест2/Глава 5. Итоговый тест2.cpp
+++ Глава 5. Итоговый тест2/Глава 5. Итоговый тест2.cpp
@@ -7,14 +7,14 @@
 #include <cstdlib>
 #include <ctime>
 
-int getRandomNumber(int min, int max) // Нужно делать const int&. Ну ок, ты ссылки еще не знаешь, но const то знаешь)))) И поделай функции const
+int getRandomNumber(int min, int max) // Нужно делать const int&. Ну ок, ты ссылки еще не знаешь, но const то знаешь)))) И поделай функции const. Чего в параметрах не const принимаешь?
 {
     static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
    
     return static_cast<int>(rand() * fraction * (max - min + 1) + min);
 }
 
-bool playGame(int randNumb, int guesses) // Тоже const&
+bool playGame(int randNumb, int guesses) // Тоже const&. Чего в параметрах не const принимаешь?
 {
     for (int i = 1; i <= guesses; ++i)
     {
@@ -38,7 +38,7 @@ bool playGame(int randNumb, int guesses) // Тоже const&
     return false;
 }
 
-bool requestForReplay() // тоже const&. Или указатели но & проще
+bool requestForReplay() // тоже const&. Или указатели но & проще. Ты возвращаешь char а функция должна возвращать bool?
 {
     char letter;
     do
@@ -54,16 +54,16 @@ int main()
     srand(static_cast<unsigned int>(time(0))); // устанавка значения системных часов в качестве стартового числа
     rand(); // Первый вызов rand() для сброса результата этой функции (для большего отличия первого рандомного числа от стартового)
     
-    const int guesses = 7;
+    const int guesses = 7; //Почему тут обьявляешь? ты же ее не тут используешь, обьявляй там где ипсользуешь
         
     do
     {
-        int randNumb = getRandomNumber(1, 100);
+        int randNumb = getRandomNumber(1, 100); // Почему тут обьявляешь? ты же ее не тут используешь, обьявляй там где ипсользуешь
 
         std::cout << "Let's play a game. I'm thinking of a number. You have " << guesses << " tries to guess what it is.\n";
 
         bool won = playGame(randNumb, guesses);
-        if (won)
+        if (won) // можно написать if(playGame(randNumb, guesses)), если тебе не нужна больше переменная won
         {
             std::cout << "Correct! You win!\n";
         }
